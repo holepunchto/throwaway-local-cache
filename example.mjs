ccb6d2a0587451743257caac375e5a844e69a42b
rafapaezbas <rafael.paez@tether.io>
2026-07-03 20:04:28 +0200

    updated example

--- example.mjs
+++ example.mjs
@@ -1,11 +1,21 @@
+import fs from 'fs/promises'
 import Cache from './index.js'
 
-const c = new Cache('cache')
+const c = new Cache('cache.json', {
+  async load (folder) {
+    try {
+      return JSON.parse(await fs.readFile(folder))
+    } catch {
+      return null // nothing persisted yet
+    }
+  },
+  async save (folder, data) {
+    await fs.writeFile(folder, data)
+  }
+})
 
 const value = await c.get('hello')
 
 c.queuePut('hello', 'world2')
 
-setInterval(() => {}, 1000)
-
-console.log(value)
+await c.flush() // force persist it now
